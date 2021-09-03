37f9ad4dfd1360679bbc6128e0d8d670cce9ee10
Michael Trevino <mtrevin93@gmail.com>
2021-09-03 12:38:34 -0400

    updated onehigherplayer to work when either player's play method is invoked

--- OneHigherPlayer.cs
+++ OneHigherPlayer.cs
@@ -7,6 +7,10 @@ namespace ShootingDice
     // Override the Play method to make a Player who always roles one higher than the other player
     public class OneHigherPlayer : Player
     {
+        public int Roll (int otherRoll)
+        {
+            return otherRoll + 1;
+        }
         public override void Play(Player other)
         {
             // Call roll for "this" object and for the "other" object
--- Player.cs
+++ Player.cs
@@ -13,14 +13,24 @@ namespace ShootingDice
             return new Random().Next(DiceSize) + 1;
         }
 
+        public virtual string Flavor()
+        {
+            return "";
+        }
+
         public virtual void Play(Player other)
         {
             // Call roll for "this" object and for the "other" object
             int myRoll = Roll();
             int otherRoll = other.Roll();
-
+            if (other is OneHigherPlayer)
+            {
+                otherRoll = (other as OneHigherPlayer).Roll(myRoll);
+            }
             Console.WriteLine($"{Name} rolls a {myRoll}");
+            Flavor();
             Console.WriteLine($"{other.Name} rolls a {otherRoll}");
+            other.Flavor();
             if (myRoll > otherRoll)
             {
                 Console.WriteLine($"{Name} Wins!");
